9fb610b9565520eaaaf8af189884591b4d1bd18f
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-24 07:15:22 +0200

    Refactor and codewars submission

--- src/smallerThanMe.js
+++ src/smallerThanMe.js
@@ -1,7 +1,5 @@
 function smallerThanMe(nums) {
-
   let result = Array(nums.length).fill(0);
-
   for (let i = 0; i < nums.length; i++){
     for (let j = i +1; j < nums.length; j++){
       if (nums[i] > nums[j]) {
@@ -9,17 +7,8 @@ function smallerThanMe(nums) {
       }
     }
   }
-  console.log('result: ', result)
   return result;
 }
 
-function arrayIsEqual (arr1, arr2){
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1.length !== arr2.length) return false;
-    if (arr1[i] !== arr2[i]) return false
-  }
-
-  return true;
-}
 
 module.exports = smallerThanMe;
